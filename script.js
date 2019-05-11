d9d9363f17b3647df76695886a1f2c1950b8db40
Kwei-Gang Lai <lai225joseph@gmail.com>
2019-05-11 15:59:11 +0800

    fix input value

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-const rgbR = document.getElementById("rgbR").value
-const rgbG = document.getElementById("rgbG").value
-const rgbB = document.getElementById("rgbB").value
+
 const example = document.querySelector("#example")
 const button = document.querySelector("button")
 
 button.addEventListener("click", (e) => {
+  const rgbR = document.querySelector("#rgbR").value
+  const rgbG = document.querySelector("#rgbG").value
+  const rgbB = document.querySelector("#rgbB").value
   const network = new brain.NeuralNetwork()
   network.train([
     { input: { r: 0.62, g: 0.72, b: 0.88 }, output: { light: 1 } },
@@ -15,7 +16,7 @@ button.addEventListener("click", (e) => {
     { input: {r: 1, g: 0.99, b: 0}, output: { light: 1 } },
     { input: {r: 1, g: 0.42, b: 0.52}, output: { dark: 1 } },
   ])
-
+  
   const result = network.run({r: rgbR, g: rgbG, b: rgbB})
   console.log(result);
   example.style.background = e.target.value
